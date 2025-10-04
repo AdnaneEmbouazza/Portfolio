990f623e9b736c812c1c0506595d3fb2c1ce546b
AdnaneEmbouazza <adnaneembouazza77000@gmail.com>
2025-10-05 01:17:45 +0200

    completion rubrique projets

--- js/scroll.js
+++ js/scroll.js
@@ -1,17 +1,17 @@
-(function () {
-      const scroller = document.getElementById('projects-scroller');
-      const btns = document.querySelectorAll('.project-scroll-btn');
-      if (!scroller) return;
-      const scrollByPage = (dir) => {
-        const card = scroller.querySelector('.project-card');
-        const gap = parseInt(getComputedStyle(scroller).gap) || 16;
-        const step = (card ? card.offsetWidth : scroller.clientWidth) + gap;
-        scroller.scrollBy({ left: dir * step, behavior: 'smooth' });
+(function () { // pour executer la fonction imédiatement sans appels
+      const scroller = document.getElementById('projects-scroller');  // récupére l'element qui contient les projets
+      const btns = document.querySelectorAll('.project-scroll-btn'); // récupère les 2 boutons droite/gauche
+      if (!scroller) return; // arrete le script si le conteneur n'existe pas ou n'est pas trouvé 
+      const scrollByPage = (dir) => { // scroll en pas en fonction de dir (1 , -1)
+        const card = scroller.querySelector('.project-card'); // récupére le premier projet comme référence (premier enfant de la classe .projet-card)
+        const step = (card ? card.offsetWidth : scroller.clientWidth) ; // distance à scroller card.offsetwidth(largeur du container du projets)
+        scroller.scrollBy({ left: dir * step, behavior: 'smooth' }); //deplace scroll horizontalement step , DIR (1 pour scroll droit et -1 pour scoll gauche) ,smooth(défilemnent fluide)
       };
-      btns.forEach(b => b.addEventListener('click', () => scrollByPage(Number(b.dataset.dir))));
+
+      btns.forEach(b => b.addEventListener('click', () => scrollByPage(Number(b.dataset.dir)))); // listener pour chaque bouton qui appelle scrollBypage()avec la valeur de dir correspondante(1 pour btn droit et -1 pour btn gauche)
       // optionnel : clavier ← → pour accessibility
-      window.addEventListener('keydown', (e) => {
-        if (e.key === 'ArrowLeft') scrollByPage(-1);
-        if (e.key === 'ArrowRight') scrollByPage(1);
+      window.addEventListener('keydown', (e) => { // listener pour touche de clavier
+        if (e.key === 'ArrowLeft') scrollByPage(-1); //scroll gauche pour fleche gauche
+        if (e.key === 'ArrowRight') scrollByPage(1); //scroll droit pour fleche droite
       });
     })();
